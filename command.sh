f50f6f8a49756036779de7eeceef268f80615ff7
Arxel <1amitpandit2468@gamil.com>
2023-09-05 17:08:34 +0530

    Updated command.sh

--- command.sh
+++ command.sh
@@ -1,1 +1,28 @@
-REPO_URL https://github.com/arxel2468/react-app-forage
+
+# Step 1: Create React App
+npx create-react-app my-app
+
+
+cd my-app
+git init
+git add .
+git commit -m "Initial commit"
+gh repo create my-app
+
+
+git checkout -b update_logo
+
+
+git add .
+git commit -m "Updated logo and link"
+git push origin update_logo
+
+
+gh pr create --base master --head update_logo --title "Update Logo and Link"
+
+
+# Step 8: Merge PR
+# ... (add any relevant commands)
+
+# Repository URL
+REPO_URL https://github.com/yourusername/my-app
